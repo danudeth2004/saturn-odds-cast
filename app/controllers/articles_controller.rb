fea9753324db14476bd1d1013c1ecb3ff4ed2beb
danudeth2004 <oat25482547@gmail.com>
2025-08-09 14:30:06 +0700

    feat: Admin can crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,42 +20,42 @@ class ArticlesController < ApplicationController
   end
 
   # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
+  # def create
+  #   @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @article.save
+  #       format.html { redirect_to @article, notice: "Article was successfully created." }
+  #       format.json { render :show, status: :created, location: @article }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @article.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /articles/1 or /articles/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @article.update(article_params)
+  #       format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
+  #       format.json { render :show, status: :ok, location: @article }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @article.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article.destroy!
+  # # DELETE /articles/1 or /articles/1.json
+  # def destroy
+  #   @article.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to articles_path, notice: "Article was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to articles_path, notice: "Article was successfully destroyed.", status: :see_other }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
---
+++ app/views/admin/articles/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @articles, partial: "admin/articles/admin_article", as: :admin_article
